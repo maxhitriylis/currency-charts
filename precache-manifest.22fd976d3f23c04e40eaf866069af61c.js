d0a1a02debf3a49ba4933e6a6a9bd7dbab70564d
makslis <lisin.m@future-group.ru>
2020-07-31 22:57:36 +0300

    Updates

---
+++ precache-manifest.22fd976d3f23c04e40eaf866069af61c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1447f80777b06be64b271a4d3679801c",
+    "url": "/currency-charts/index.html"
+  },
+  {
+    "revision": "614e752123471aef5dfa",
+    "url": "/currency-charts/static/css/main.d23177b3.chunk.css"
+  },
+  {
+    "revision": "7444d3054c559276fbac",
+    "url": "/currency-charts/static/js/2.46500fd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-charts/static/js/2.46500fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614e752123471aef5dfa",
+    "url": "/currency-charts/static/js/main.5e93da2c.chunk.js"
+  },
+  {
+    "revision": "9d37833301f41700958a",
+    "url": "/currency-charts/static/js/runtime-main.e02248e3.js"
+  }
+]);
